3260a9d3775c64d273ffe67c25fc08c3a8bf2cee
Nguyễn Ngọc Tín <131370934+ngoctinn@users.noreply.github.com>
2024-02-28 15:21:46 +0700

    Add project files.

---
+++ Phần mềm quản lý kho WPF - MVVM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Phần_mềm_quản_lý_kho_WPF___MVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Phần mềm quản lý kho WPF - MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using QuanLyKho.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Phần_mềm_quản_lý_kho_WPF___MVVM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này 
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel");
+        }
+    }
+}
